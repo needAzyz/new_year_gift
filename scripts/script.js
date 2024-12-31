bff711e30f38359b3af6ac7812a79b8e3d8d6e68
Azyz <contact.azyz@gmail.com>
2024-12-31 02:26:43 +0100

    yey
    happy new year

---
+++ scripts/script.js
@@ -0,0 +1,48 @@
+function showLocationAndIPDetails() {
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth();
+  let day = date.getDate();
+  if (year === 2025 && month === 0 && day === 1) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          fetch("http://ip-api.com/json/")
+            .then((response) => response.json())
+            .then((data) => {
+              const details = `ur country: ${data.country}`;
+              const combinedInfo = `ur location:\n${details} \n5tot tol: ${latitude}\n5tot 3orth: ${longitude}\nIP address: ${data.query}\nu got hacked\nso next time accept ma gift okeh?`;
+              alert(combinedInfo);
+            })
+            .catch((error) => {
+              console.error("fetching IP address:", error);
+              alert("is this ur IP address?  196.237.117.245\nu got hacked\n i knoz where u live \n so next time accept ma gift okeh?");
+            });
+        },
+        (error) => {
+          console.error("error getting ur location:", error);
+          alert("open ur location dont worry am not hacking u");
+        }
+      );
+    } else {
+      alert("change ur browser to not safe browser.");
+    }
+  } else {
+    alert("wait for the first of january");
+  }
+}
+document
+  .getElementById("showLocationAndIPButton")
+  .addEventListener("click", showLocationAndIPDetails);
+function gift() {
+  date = new Date();
+  year = date.getFullYear();
+  month = date.getMonth();
+  day = date.getDate();
+  if (year === 2025 && month === 0 && day === 1) {
+    window.location.href = "views/giftpage.html";
+  } else {
+    alert("wait for the first of january");
+  }
+}
